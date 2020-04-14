aa4fce851c592aba0104c6100b4a4d51084898e3
soi02 <soi02@DESKTOP-JD8FE2E>
2020-04-14 14:16:29 +0900

    아모르파티

--- src/com/kh/maskRush/model/dao/states/Run2.java
+++ src/com/kh/maskRush/model/dao/states/Run2.java
@@ -1,7 +1,9 @@
 package com.kh.maskRush.model.dao.states;
 
+
 public class Run2 {
-//장소이 test용 run 클래스
+	//장소이 test용 run 클래스
+	
 	
 	public static void main(String[] args) {
 		new FirstMenuChoice();
