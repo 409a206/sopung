dc4d4ec5433acfd269ab02b2cb8937a1f32af05d
DaeunSim <daeunnsim@gmail.com>
2020-04-15 20:00:43 +0900

    편의점 추가
    클래스, 사진 추가

--- src/com/kh/maskRush/model/dao/states/InConvenienceStore.java
+++ src/com/kh/maskRush/model/dao/states/InConvenienceStore.java
@@ -1,6 +1,7 @@
 package com.kh.maskRush.model.dao.states;
 
 import java.awt.Color;
+
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
@@ -9,11 +10,6 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import com.kh.maskRush.model.dao.entities.Creature.Player;
-
-import Convenienve.Contalk;
-import Convenienve.FirstMenuChoice;
-import Convenienve.InConvenienceStore2;
-import Convenienve.changePanel;
 
 public class InConvenienceStore extends JPanel {
 
@@ -29,7 +25,7 @@ public class InConvenienceStore extends JPanel {
 
 		JLabel mask = new JLabel();
 		mask.setBounds(33, 316, 90, 72);
-		mask.setIcon(new ImageIcon(InConvenienceStore.class.getResource("/Convenienve/mask.gif")));
+		mask.setIcon(new ImageIcon(InConvenienceStore.class.getResource("src/InConvenience/mask.gif")));
 		add(mask);
 
 		JPanel boyPanel = new JPanel();
@@ -54,12 +50,11 @@ public class InConvenienceStore extends JPanel {
 					boyPanel.setLocation(boyPanel.getX() - FLY_UNIT, boyPanel.getY());
 					break;
 
-					
 				}
 				
 				if(keyCode == KeyEvent.VK_SPACE) {
 					if(boyPanel.getX() <= 99 && boyPanel.getX() >= 66) {
-						ChangePanel.changePanel(mainChoice, panel, new Contalk(mainChoice));
+						ChangePanel.changePanel(mainChoice, panel, new InConvenienceStore_talk(mainChoice));
 					}
 				}
 				
@@ -72,19 +67,19 @@ public class InConvenienceStore extends JPanel {
 
 		JLabel boy = new JLabel();
 		boyPanel.add(boy);
-		boy.setIcon(new ImageIcon(InConvenienceStore.class.getResource("/Convenienve/maskwithboy.png")));
+		boy.setIcon(new ImageIcon(InConvenienceStore.class.getResource("src/InConvenience/maskwithboy.png")));
 		boy.setVisible(true);
 		
 
 		JLabel cu = new JLabel();
-		cu.setIcon(new ImageIcon(InConvenienceStore.class.getResource("/Convenienve/\uC54C\uBC14\uC0DD.png")));
+		cu.setIcon(new ImageIcon(InConvenienceStore.class.getResource("src/InConvenience/알바생.png")));
 		cu.setBounds(447, 22, 129, 135);
 		add(cu);
 
 		// 배경라벨
 		JLabel back = new JLabel();
 		// boy.setLabelFor(back);
-		back.setIcon(new ImageIcon(InConvenienceStore.class.getResource("/Convenienve/map.png")));
+		back.setIcon(new ImageIcon(InConvenienceStore.class.getResource("src/InConvenience/map.png")));
 		back.setBounds(14, 0, 858, 629);
 		this.add(back);
 
