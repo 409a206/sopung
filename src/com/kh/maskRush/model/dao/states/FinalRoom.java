c325a6bbf4c464df533f6a8899c6b5b05c7b1cf8
SSE <SSE@DESKTOP-9E2DCVI>
2020-04-16 11:42:45 +0900

    마지막방 대화 추가, 마스크미니게임 설명, 종료시 화면 추가

---
+++ src/com/kh/maskRush/model/dao/states/FinalRoom.java
@@ -0,0 +1,90 @@
+package com.kh.maskRush.model.dao.states;
+
+import java.awt.EventQueue; 
+import java.awt.Font;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextPane;
+import javax.swing.border.EmptyBorder;
+
+
+
+public class FinalRoom extends JFrame {
+
+   private JPanel contentPane;
+
+   public static void main(String[] args) {
+
+      EventQueue.invokeLater(new Runnable() {
+         public void run() {
+            try {
+               FinalRoom frame = new FinalRoom();
+               frame.setVisible(true);
+            } catch (Exception e) {
+               e.printStackTrace();
+            }
+         }
+      });
+   }
+
+
+
+   public FinalRoom() {
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      setBounds(0, 0, 800, 600);
+      contentPane = new JPanel();   
+      contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+      setContentPane(contentPane);
+      contentPane.setLayout(null);
+
+      JTextPane textPane = new JTextPane();
+      textPane.setEditable(false); // 편집불가.
+      String[] busChat = {"오 광택나는  진호야!!\r\n 마스크는 다 구했니?","네 엄마~! 4개 다 구했어요!\r\n"
+      		+ "그리고 저 사실...","응 말해보렴 ^^", "위험에 처한 시민들을 구해주는\r\n 멋진 경찰이 되고싶어요."};
+      
+
+      textPane.addKeyListener(new KeyAdapter() {
+
+         int i = 0;
+         @Override
+         public void keyPressed(KeyEvent e) {
+
+            if(i < busChat.length) {
+            if(e.getKeyCode() == KeyEvent.VK_SPACE) {
+               textPane.setText(busChat[i]);
+            }
+         }
+         }
+
+         @Override
+         public void keyReleased(KeyEvent e) {
+            i++;
+         }
+
+      });
+
+
+
+
+      contentPane.add(textPane);
+      textPane.setFont(new Font("둥근모꼴", Font.PLAIN, 24));
+      textPane.setBounds(88, 400, 446, 127);
+      
+      JLabel talk = new JLabel();
+      talk.setIcon(new ImageIcon(InPharmacy.class.getResource("/textures/대화창2.png")));
+      talk.setBounds(-10, -25, 800, 600);
+      this.add(talk);
+
+      JLabel backGround = new JLabel();
+      backGround.setIcon(new ImageIcon(InPharmacy.class.getResource("/textures/finalRoom1.png")));
+      backGround.setBounds(-16, -15, 800, 600);
+      this.add(backGround);
+      
+   }
+
+}
---
+++ src/com/kh/maskRush/model/dao/states/MaskMiniRestart.java
@@ -0,0 +1,89 @@
+package com.kh.maskRush.model.dao.states;
+
+import java.awt.EventQueue; 
+import java.awt.Font;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextPane;
+import javax.swing.border.EmptyBorder;
+
+
+
+public class MaskMiniExplain1 extends JFrame {
+
+   private JPanel contentPane;
+
+   public static void main(String[] args) {
+
+      EventQueue.invokeLater(new Runnable() {
+         public void run() {
+            try {
+               MaskMiniExplain1 frame = new MaskMiniExplain1();
+               frame.setVisible(true);
+            } catch (Exception e) {
+               e.printStackTrace();
+            }
+         }
+      });
+   }
+
+
+
+   public MaskMiniExplain1() {
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      setBounds(0, 0, 800, 600);
+      contentPane = new JPanel();   
+      contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+      setContentPane(contentPane);
+      contentPane.setLayout(null);
+
+//      JTextPane textPane = new JTextPane();
+//      textPane.setEditable(false); // 편집불가.
+//      String[] busChat = {"편의점으로 들어가시겠습니까?","(Y/N)\r\n"};
+//      
+//
+//      textPane.addKeyListener(new KeyAdapter() {
+//
+//         int i = 0;
+//         @Override
+//         public void keyPressed(KeyEvent e) {
+//
+//            if(i < busChat.length) {
+//            if(e.getKeyCode() == KeyEvent.VK_SPACE) {
+//               textPane.setText(busChat[i]);
+//            }
+//         }
+//         }
+//
+//         @Override
+//         public void keyReleased(KeyEvent e) {
+//            i++;
+//         }
+//
+//      });
+
+
+
+
+//      contentPane.add(textPane);
+//      textPane.setFont(new Font("둥근모꼴", Font.PLAIN, 24));
+//      textPane.setBounds(88, 400, 446, 127);
+//      
+//      JLabel talk = new JLabel();
+//      talk.setIcon(new ImageIcon(InPharmacy.class.getResource("/textures/대화창2.png")));
+//      talk.setBounds(-10, -25, 800, 600);
+//      this.add(talk);
+
+      JLabel backGround = new JLabel();
+      backGround.setIcon(new ImageIcon(InPharmacy.class.getResource("/textures/maskMiniRestart.png")));
+      backGround.setBounds(-16, -15, 800, 600);
+      this.add(backGround);
+      
+   }
+
+}
